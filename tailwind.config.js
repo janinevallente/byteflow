4d188000cfac742f3f99738d48cb41197e5751db
janinevallente <janinexvallente@gmail.com>
2026-06-12 17:33:46 +0800

    enh: updated accent in colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ export default {
         backgroundColor: '#0f1015',
         backgroundCard: '#16171d',
         borderColor: '#2e303a',
-        accent: '#c084fc',
+        accent: '#9459d0',
         accentBg: 'rgba(192, 132, 252, 0.1)',
         accentBorder: 'rgba(192, 132, 252, 0.4)',
         shadowColor: 'rgba(0, 0, 0, 0.4) 0 4px 24px, rgba(0, 0, 0, 0.2) 0 1px 4px',
